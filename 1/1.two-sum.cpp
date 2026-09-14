03df183db7cf25d99004b8d5c2d5a676ee65e650
mr Rikam <rikamgouda99@gmail.com>
2026-09-14 09:45:29 +0530

    Leetcode #1 Add Two Sum solution

---
+++ 1/1.two-sum.cpp
@@ -0,0 +1,18 @@
+//Using c++ 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int,int>hash;
+     for(int i=0;i<nums.size();i++)
+     {
+        int need=target-nums[i];
+        if(hash.find(need)!=hash.end())
+           return {hash[need],i};
+        else 
+          hash[nums[i]]=i;
+     }
+     return {};
+    }
+};      
